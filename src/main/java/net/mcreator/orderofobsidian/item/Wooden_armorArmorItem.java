5602b927e981e37b43d8926cbfe81118757ae19c
klaivertt <Scryl@PCALEX>
2021-11-28 17:26:30 +0100

    vesion

--- src/main/java/net/mcreator/orderofobsidian/item/Wooden_armorArmorItem.java
+++ src/main/java/net/mcreator/orderofobsidian/item/Wooden_armorArmorItem.java
@@ -69,7 +69,7 @@ public abstract class Wooden_armorArmorItem extends ArmorItem {
 
 		@Override
 		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
-			return "orderofobsidian:textures/models/armor/wooden_armor_layer_1.png";
+			return "orderofobsidian:textures/models/armor/wooden_armor__layer_1.png";
 		}
 	}
 
@@ -81,7 +81,7 @@ public abstract class Wooden_armorArmorItem extends ArmorItem {
 
 		@Override
 		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
-			return "orderofobsidian:textures/models/armor/wooden_armor_layer_1.png";
+			return "orderofobsidian:textures/models/armor/wooden_armor__layer_1.png";
 		}
 	}
 
@@ -93,7 +93,7 @@ public abstract class Wooden_armorArmorItem extends ArmorItem {
 
 		@Override
 		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
-			return "orderofobsidian:textures/models/armor/wooden_armor_layer_2.png";
+			return "orderofobsidian:textures/models/armor/wooden_armor__layer_2.png";
 		}
 	}
 
@@ -105,7 +105,7 @@ public abstract class Wooden_armorArmorItem extends ArmorItem {
 
 		@Override
 		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
-			return "orderofobsidian:textures/models/armor/wooden_armor_layer_1.png";
+			return "orderofobsidian:textures/models/armor/wooden_armor__layer_1.png";
 		}
 	}
 }
